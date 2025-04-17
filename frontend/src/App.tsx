ce14c1f7feb868db31688034164ab9511177f027
Sunny Kumar <goodluck14115@gmail.com>
2025-04-17 16:04:34 +0530

    feat: create Vite + React app & setup TailwindCSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-gray-500">Issue Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
