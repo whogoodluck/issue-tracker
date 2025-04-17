2dffcb66a72270132f78810b904937c6cbf07db8
Sunny Kumar <goodluck14115@gmail.com>
2025-04-17 16:30:47 +0530

    chore: setup Shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
-    <div className='flex h-screen items-center justify-center'>
+    <div className='flex h-screen flex-col items-center justify-center'>
       <h1 className='text-3xl font-bold text-gray-500'>Issue Tracker</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
