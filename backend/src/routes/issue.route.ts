d298af8dad65d5ca8cd286f680d297146f83b957
Sunny Kumar <goodluck14115@gmail.com>
2025-04-18 20:48:15 +0530

    fix: build error

--- backend/src/routes/issue.route.ts
+++ backend/src/routes/issue.route.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express'
+import { RequestHandler, Router } from 'express'
 import issueController from '../controllers/issue.controller'
 
 const issueRoutes = Router()
 
 issueRoutes.post('/', issueController.createNewIssue)
-issueRoutes.get('/', issueController.getAllIssues)
+issueRoutes.get('/', issueController.getAllIssues as RequestHandler)
 issueRoutes.get('/:id', issueController.getIssueById)
 issueRoutes.delete('/:id', issueController.deleteIssueById)
 issueRoutes.put('/:id', issueController.updateIssueById)
